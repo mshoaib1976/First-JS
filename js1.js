e59983c46b89c2e9d89a2e9dd7b17ecb28a6b682
kashif.raza <kashif.raza@integrationxperts.com>
2020-08-19 13:10:18 +0500

    if and switch in JS

---
+++ js1.js
@@ -0,0 +1,43 @@
+//alert('hello world');
+
+var a = 10 ;
+var b = 10;
+var c=a+b;
+
+if(a>b)
+{
+    document.write("a is greater than b");
+}
+else if(a===b)
+{
+    document.write("A equal to B");
+}
+else
+{
+    document.write("b is greater than a ");
+}
+
+document.write("<br>");
+
+document.write("-------------------------")
+document.write("<br>");
+switch(b+a)
+{
+    case 10 : document.write("its 10 case");
+    break;
+    case 20 : document.write("its 20 case");
+    break;
+    case 30 : document.write('')
+}
+document.write("<br/>");
+document.write("-------------------------")
+document.write("<br>");
+var i =4;
+while(i<20){
+    document.write("*"+i);
+    i++;
+    document.write("<br/>");
+
+}
+
+
